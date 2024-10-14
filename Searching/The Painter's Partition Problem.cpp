7685b7ad66db1f0b90b0764fc1663149642449d6
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-14 20:03:59 +0530

    Update The Painter's Partition Problem.cpp

--- Searching/The Painter's Partition Problem.cpp
+++ Searching/The Painter's Partition Problem.cpp
@@ -46,40 +46,50 @@ class Solution
 class Solution
 {
   public:
-  long long countPainters(int arr[], int n,int time)
-  {
-      int painters = 1;
-      long long boardPainters = 0;
-      for(int i =0;i<n;i++)
-      {
-          if(arr[i] + boardPainters <= time)
-          {
-              boardPainters += arr[i];
-          }
-          else{
-              painters++;
-              boardPainters = arr[i];
-          }
-      }
-      return painters;
-  }
+ 
+int solve(int arr[],int n, int time,int k) {
+    
+    int painters = 1;
+    long long boardsPainter = 0;
+    for (int i = 0; i < n; i++) {
+        if (boardsPainter + arr[i] <= time) {
+            //allocate board to current painter
+            boardsPainter += arr[i];
+        }
+        else {
+            //allocate board to next painter
+            painters++;
+            boardsPainter = arr[i];
+        }
+    }
+    return (painters <= k) ? 1 : 0 ;
+}
+
+    
+  public:
     long long minTime(int arr[], int n, int k)
     {
-    int low = *max_element(arr, arr+n);
-    int high = accumulate(arr, arr+n, 0);
-   while(low <= high)
-   {
-       int mid = low+(high-low)/2;
-       int painters = countPainters(arr,n,mid);
-       if(painters > k)
-       {
-           low = mid+1; //min(max)
-       }
-       else{
-           high = mid-1;
-       }
-   }
-   return low;
+         long long sum=0;
+         int ans=-1;
+        for (int i=0;i<n;i++){
+          sum += arr[i];
+        }
+        
+        long long low=*max_element(arr,arr+n);
+        long long high = sum;
+         
+        while (low<=high)
+        {
+             long long mid=low+(high-low)/2;
+        if(solve(arr,n,mid,k) == 1)
+        {
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+        }
+        return ans;
     }
 };
-
