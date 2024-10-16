24d7a5dbdc0d400f750e0e2af29222a4462aca7d
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-16 20:32:47 +0530

    Create Gas Station.cpp

---
+++ Stack and Queue/Gas Station.cpp
@@ -0,0 +1,21 @@
+### Approach 1: Greedy
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int fuel=0, total=0;
+        int ans=0;
+    for(int i=0;i<gas.size();i++){
+         fuel += gas[i] - cost[i];
+        total += gas[i] - cost[i];
+        if(fuel < 0){
+            ans = i+1;
+            fuel=0;
+        }
+    }
+    return (total >= 0) ? ans : -1;
+    }
+};
