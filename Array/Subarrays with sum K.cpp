de9e03520854c6d4af99992694b8d7deb141b76f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-21 20:51:11 +0530

    Update Subarrays with sum K.cpp

--- Array/Subarrays with sum K.cpp
+++ Array/Subarrays with sum K.cpp
@@ -23,3 +23,27 @@ public:
 };
 
 ### Approach 2: 
+
+Time Complexity: O(n) 
+Auxiliary Space: O(n)
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n= nums.size();
+        unordered_map<int,int> mp;
+        int count =0;
+        mp.insert({0,1});
+    int sum = 0; //prefix sum
+    for(int i =0;i<n;i++)
+    {
+        sum += nums[i];
+        if(mp.find(sum-k) != mp.end())
+        {
+            count += mp[sum-k];
+        }
+        mp[sum]+=1; //add or update in map
+    }
+    return count;
+    }
+};
