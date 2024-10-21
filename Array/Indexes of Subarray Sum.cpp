0b72ce8f8e6287619eabd63dedf3d6da98f60b0a
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-10-21 19:06:56 +0530

    Update Indexes of Subarray Sum.cpp

--- Array/Indexes of Subarray Sum.cpp
+++ Array/Indexes of Subarray Sum.cpp
@@ -1,3 +1,8 @@
+### Approach 1: Brute Force
+
+Time Complexity: O(n^2), Trying all subarrays from every index, used nested loop for the same
+Auxiliary Space: O(1)
+
 class Solution {
   public:
     vector<int> subarraySum(vector<int> &arr, int target) {
@@ -33,5 +38,40 @@ class Solution {
               return ans;
           }
           return {-1};
+    }
+};
+
+### Approach 2: Expected 
+
+Time Complexity: O(n)
+Auxiliary Space: O(1). Since no extra space has been taken.
+  
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+      if(target == 0) {
+          return {-1};
+      }
+      int n = arr.size();
+     int start = 0, end = 0;
+     long long windowSum = arr[0];
+     while(end < n)
+     {
+         if(windowSum < target)
+         {
+             end++;
+             windowSum += arr[end];
+         }
+         else if(windowSum == target)
+         {
+             return {start+1,end+1};
+         }
+         else if(windowSum > target)
+         {
+             windowSum -= arr[start];
+             start++;
+         }
+     }
+     return {-1};
     }
 };
